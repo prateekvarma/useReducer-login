8692b9376d081b7f17cd441860e765d979f6bb4f
Prateek Varma <prtk.varma@gmail.com>
2022-08-28 12:29:46 -0600

    Passed the logout function - as a Context and accessed it using the useContext() hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import AuthContext from '../../context/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
 
   return (
@@ -20,7 +20,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {} //adding the function is here is not necessary, but it's good practice. Same applies for the 'isLoggedIn'. These values are kind of the init values.
 });
 
 export default AuthContext;
