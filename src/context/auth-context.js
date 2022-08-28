0e533247e3a7793118cdbae17a9c41bf75486df7
Prateek Varma <prtk.varma@gmail.com>
2022-08-28 15:31:52 -0600

    Moved all auth state management, and Context logic to it's own component - keeping the App component lean

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false,
-    onLogout: () => {} //adding the function is here is not necessary, but it's good practice. Same applies for the 'isLoggedIn'. These values are kind of the init values.
+  isLoggedIn: false,
+  onLogout: () => {}, //adding the function is here is not necessary, but it's good practice. Same applies for the 'isLoggedIn'. These values are kind of the init values.
+  onLogin: (email, password) => {}
 });
+
+export const AuthContentProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { AuthContentProvider } from './context/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <AuthContentProvider>
+        <App />
+    </AuthContentProvider>
+);
