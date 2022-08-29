cdbb86dfa1e4a34b4e43611d2d0fdf18ffa43adb
Prateek Varma <prtk.varma@gmail.com>
2022-08-29 13:48:54 -0600

    Used React.forwardRef and useImperativeHandle hook to trigger a function from a child component - which sets focus on an input field

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -35,6 +35,9 @@ const Login = (props) => {
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -84,13 +87,23 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    //
+    if(formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+      // form is not valid, set focus on the first input field
+    } else if (!emailState.isValid) {
+      emailInputRef.current.theFocus(); //the TheFocus comes from the Input component
+    } else {
+      // here, the password is invalid
+      passwordInputRef.current.theFocus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input 
+          ref={emailInputRef}
           id="email" 
           label="E-Mail" 
           type="email" 
@@ -100,6 +113,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler} 
         />
         <Input 
+          ref={passwordInputRef}
           id="password" 
           label="Password" 
           type="password" 
@@ -109,7 +123,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler} 
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
